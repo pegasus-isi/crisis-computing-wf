d0249e9580bea92eda5e3f33dfcd407ade6c6be1
Shubham Nagarkar <slnagark@tanaka-isi-edu.ads.isi.edu>
2021-04-27 23:21:29 -0700

    supcon

---
+++ bin/main_supcon.py
@@ -0,0 +1,325 @@
+#!/usr/bin/env python3
+
+
+from __future__ import print_function
+
+import os
+import sys
+import argparse
+import time
+import math
+import glob
+import tensorboard_logger as tb_logger
+import torch
+import torch.backends.cudnn as cudnn
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader, Dataset
+from supcon_util import TwoCropTransform, AverageMeter
+from supcon_util import adjust_learning_rate, warmup_learning_rate
+from supcon_util import set_optimizer, save_model
+from resnet_big import SupConResNet
+from losses import SupConLoss
+from PIL import Image
+import re
+
+try:
+    import apex
+    from apex import amp, optimizers
+except ImportError:
+    pass
+
+MEAN = (0.4905, 0.4729, 0.4560) 
+STD = (0.2503, 0.2425, 0.2452)
+CHECKPOINT =  'checkpoint_supcon.pth'
+FINAL_OUTPUT = 'supcon_final_model.pth'
+
+def parse_option():
+    parser = argparse.ArgumentParser('argument for training')
+
+    parser.add_argument('--print_freq', type=int, default=10,
+                        help='print frequency')
+    parser.add_argument('--save_freq', type=int, default=50,
+                        help='save frequency')
+    parser.add_argument('--batch_size', type=int, default=2,
+                        help='batch_size')
+    parser.add_argument('--num_workers', type=int, default=2,
+                        help='num of workers to use')
+    parser.add_argument('--epochs', type=int, default=2,
+                        help='number of training epochs')
+
+    # optimization
+    parser.add_argument('--learning_rate', type=float, default=0.05,
+                        help='learning rate')
+    parser.add_argument('--lr_decay_epochs', type=str, default='50,100,200',
+                        help='where to decay lr, can be a list')
+    parser.add_argument('--lr_decay_rate', type=float, default=0.1,
+                        help='decay rate for learning rate')
+    parser.add_argument('--weight_decay', type=float, default=1e-4,
+                        help='weight decay')
+    parser.add_argument('--momentum', type=float, default=0.9,
+                        help='momentum')
+
+    # model dataset
+    parser.add_argument('--model', type=str, default='resnet50')
+    parser.add_argument('--dataset', type=str, default='path',
+                        choices=['cifar10', 'cifar100', 'path'], help='dataset')
+    parser.add_argument('--mean', type=str, default='(0.4905, 0.4729, 0.4560)', help='mean of dataset in path in form of str tuple')
+    parser.add_argument('--std', type=str, default='(0.2503, 0.2425, 0.2452)', help='std of dataset in path in form of str tuple')
+    parser.add_argument('--data_folder', type=str, default="", help='path to custom dataset')
+    parser.add_argument('--size', type=int, default=128, help='parameter for RandomResizedCrop')
+
+    # method
+    parser.add_argument('--method', type=str, default='SupCon',
+                        choices=['SupCon', 'SimCLR'], help='choose method')
+
+    # temperature
+    parser.add_argument('--temp', type=float, default=0.07,
+                        help='temperature for loss function')
+
+    # other setting
+    parser.add_argument('--cosine', action='store_true',
+                        help='using cosine annealing')
+    parser.add_argument('--syncBN', action='store_true',
+                        help='using synchronized batch normalization')
+    parser.add_argument('--warm', action='store_true',
+                        help='warm-up for large batch training')
+    parser.add_argument('--trial', type=str, default='0',
+                        help='id for recording multiple runs')
+
+    opt = parser.parse_args()
+    
+    # check if dataset is path that passed required arguments
+    if opt.dataset == 'path':
+        assert opt.data_folder is not None \
+            and opt.mean is not None \
+            and opt.std is not None
+
+    # set the path according to the environment
+    if opt.data_folder is None:
+        opt.data_folder = './datasets/'
+    opt.model_path = './save/SupCon/{}_models'.format(opt.dataset)
+    opt.tb_path = './save/SupCon/{}_tensorboard'.format(opt.dataset)
+
+    iterations = opt.lr_decay_epochs.split(',')
+    opt.lr_decay_epochs = list([])
+    for it in iterations:
+        opt.lr_decay_epochs.append(int(it))
+
+    opt.model_name = '{}_{}_{}_lr_{}_decay_{}_bsz_{}_temp_{}_trial_{}'.\
+        format(opt.method, opt.dataset, opt.model, opt.learning_rate,
+               opt.weight_decay, opt.batch_size, opt.temp, opt.trial)
+
+    if opt.cosine:
+        opt.model_name = '{}_cosine'.format(opt.model_name)
+
+    # warm-up for large-batch training,
+    if opt.batch_size > 256:
+        opt.warm = True
+    if opt.warm:
+        opt.model_name = '{}_warm'.format(opt.model_name)
+        opt.warmup_from = 0.01
+        opt.warm_epochs = 10
+        if opt.cosine:
+            eta_min = opt.learning_rate * (opt.lr_decay_rate ** 3)
+            opt.warmup_to = eta_min + (opt.learning_rate - eta_min) * (
+                    1 + math.cos(math.pi * opt.warm_epochs / opt.epochs)) / 2
+        else:
+            opt.warmup_to = opt.learning_rate
+
+
+    return opt
+
+class DatasetLoader(Dataset):
+    def __init__(self, data, transform = None):
+        
+        self.data = data
+        self.data_len = len(data)
+        self.transform = transform
+
+    def __len__(self):
+        
+        return self.data_len
+
+    def __getitem__(self, idx):
+       
+        image = Image.open(self.data[idx]).convert('RGB')
+
+        # label 0: "Informative" label 1: "Non-Informative"
+        
+        label = int(re.findall(r'[0-9]+', self.data[idx])[-1])
+  
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+def get_data(PATH):
+    
+    images = glob.glob(PATH+'*.png') + glob.glob(PATH+'*.jpg')
+    return images
+
+def set_loader(opt):
+    # construct data loader
+    if opt.dataset == 'cifar10':
+        mean = (0.4914, 0.4822, 0.4465)
+        std = (0.2023, 0.1994, 0.2010)
+    elif opt.dataset == 'cifar100':
+        mean = (0.5071, 0.4867, 0.4408)
+        std = (0.2675, 0.2565, 0.2761)
+    elif opt.dataset == 'path':
+        mean = eval(opt.mean)
+        std = eval(opt.std)
+    else:
+        raise ValueError('dataset not supported: {}'.format(opt.dataset))
+    normalize = transforms.Normalize(mean=mean, std=std)
+
+    train_transform = transforms.Compose([
+        transforms.RandomResizedCrop(size=opt.size, scale=(0.2, 1.)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomApply([
+            transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
+        ], p=0.8),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    if opt.dataset == 'cifar10':
+        train_dataset = datasets.CIFAR10(root=opt.data_folder,
+                                         transform=TwoCropTransform(train_transform),
+                                         download=True)
+    elif opt.dataset == 'cifar100':
+        train_dataset = datasets.CIFAR100(root=opt.data_folder,
+                                          transform=TwoCropTransform(train_transform),
+                                          download=True)
+    elif opt.dataset == 'path':
+        image_data = get_data(opt.data_folder)
+        train_dataset = DatasetLoader(image_data, TwoCropTransform(train_transform))
+    else:
+        raise ValueError(opt.dataset)
+    
+
+    train_sampler = None
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=opt.batch_size, shuffle=True,
+        num_workers=opt.num_workers, pin_memory=True)
+    
+    return train_loader
+
+
+def set_model(opt):
+    model = SupConResNet(name=opt.model)
+    criterion = SupConLoss(temperature=opt.temp)
+
+    # enable synchronized Batch Normalization
+    if opt.syncBN:
+        model = apex.parallel.convert_syncbn_model(model)
+
+    if torch.cuda.is_available():
+        if torch.cuda.device_count() > 1:
+            model.encoder = torch.nn.DataParallel(model.encoder)
+        model = model.cuda()
+        criterion = criterion.cuda()
+        cudnn.benchmark = True
+
+    return model, criterion
+
+
+def train(train_loader, model, criterion, optimizer, epoch, opt):
+    """one epoch training"""
+    model.train()
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+
+    end = time.time()
+    for idx, (images, labels) in enumerate(train_loader):
+        data_time.update(time.time() - end)
+
+        images = torch.cat([images[0], images[1]], dim=0)
+        if torch.cuda.is_available():
+            images = images.cuda(non_blocking=True)
+            labels = labels.cuda(non_blocking=True)
+        bsz = labels.shape[0]
+
+        # warm-up learning rate
+        warmup_learning_rate(opt, epoch, idx, len(train_loader), optimizer)
+
+        # compute loss
+        features = model(images)
+        f1, f2 = torch.split(features, [bsz, bsz], dim=0)
+        features = torch.cat([f1.unsqueeze(1), f2.unsqueeze(1)], dim=1)
+        if opt.method == 'SupCon':
+            loss = criterion(features, labels)
+        elif opt.method == 'SimCLR':
+            loss = criterion(features)
+        else:
+            raise ValueError('contrastive method not supported: {}'.
+                             format(opt.method))
+
+        # update metric
+        losses.update(loss.item(), bsz)
+
+        # SGD
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # print info
+        if (idx + 1) % opt.print_freq == 0:
+            print('Train: [{0}][{1}/{2}]\t'
+                  'BT {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'DT {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'loss {loss.val:.3f} ({loss.avg:.3f})'.format(
+                   epoch, idx + 1, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses))
+            sys.stdout.flush()
+
+    return losses.avg
+
+
+def main():
+    opt = parse_option()
+
+    # build data loader
+    train_loader = set_loader(opt)
+
+    # build model and criterion
+    model, criterion = set_model(opt)
+
+    # build optimizer
+    optimizer = set_optimizer(opt, model)
+
+    # tensorboard
+    logger = tb_logger.Logger(logdir=opt.tb_folder, flush_secs=2)
+
+    # training routine
+    for epoch in range(1, opt.epochs + 1):
+        adjust_learning_rate(opt, optimizer, epoch)
+
+        # train for one epoch
+        time1 = time.time()
+        loss = train(train_loader, model, criterion, optimizer, epoch, opt)
+        time2 = time.time()
+        print('epoch {}, total time {:.2f}'.format(epoch, time2 - time1))
+
+        # tensorboard logger
+#         logger.log_value('loss', loss, epoch)
+#         logger.log_value('learning_rate', optimizer.param_groups[0]['lr'], epoch)
+
+        if epoch % opt.save_freq == 0:
+            torch.save(model.state_dict(), "temp_model.pth")
+            os.rename("temp_model.pth", CHECKPOINT)
+
+    # save the last model
+    torch.save(model.state_dict(), CHECKPOINT)
+    os.rename(CHECKPOINT, FINAL_OUTPUT)
+
+
+if __name__ == '__main__':
+    main()
